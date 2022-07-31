bf4429feb09c409d869cf99f2a620eeebc0f9ba9
JuanGa <juangavaldes@gmail.com>
2022-07-31 09:58:49 -0600

    fix: testimonial update

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -12,23 +12,30 @@ export const testimonialsData = [
     {
         id: 1,
         name: 'Gabriel Valdes',
-        title: 'Estudiante de Yoga y Telas',
-        text: "Excelente profesional y profesora. Clases exigentes y retadoras, siempre dispuesta a innovar y compartir su conocimiento.",
+        title: 'Estudiante de Telas y Yoga',
+        text: "Rebe es una excelente profesional y profesora. Es perfeccionista en sus clases lo cual lo convierte en un excelente reto para cualquiera. Ella es excigente y siempre hace clases retadoras, aprovechando al maximo el tiempo disponible, siempre esta dispuesta a innovar y compartir su conocimiento.",
         image: vishnu
     },
     {
         id: 2,
-        name: 'Fiorella',
+        name: 'Lucia Segura',
         title: 'Estudiante de Yoga',
-        text: "Me adecuo las clases a mis necesidades y limitaciones fisicas, gran profesional y persona.",
+        text: "Llegué a la práctica de Yoga buscando un beneficio físico y encontré un beneficio integral entre mente y cuerpo. Me encanta como la profe Rebe imparte las clases de Yoga porque es una profe comprometida con la práctica y siempre procura hacer clases variadas estimulando diferentes áreas del cuerpo y está muy atenta a corregirnos para que mejoremos cada vez, las posturas. Se nota que le pone alma a cada clase porque se siente en cada una de ellas!!!",
         image: girl2
     },
     {
         id: 3,
-        name: 'Roberto',
-        title: 'Centro de Yoga del Este',
-        text: "Excelente profesora, comprometida e innovadora. Durante el tiempo de trabajar juntos solo elogios y cumplidos hemos recibido de nuestros estudiantes.",
-        image: boy1
+        name: 'Gaudi',
+        title: 'Estudiante de Yoga',
+        text: "Las clase de Yoga, con Rebe, son  un  reto  cada  día, hay clases  en que  pienso que  no voy a  poder salir del salón por lo exigencia con la que se trabajo, pero a lo largo de  la  clase mi cuerpo  va adaptando cada asana y al finalizar, saldo del salón cargada de energía positiva  y con ganas de que llegue la próxima clase.",
+        image: girl1
+    },
+    {
+        id: 3,
+        name: 'Alejandra',
+        title: 'Estudiante de Pilates',
+        text: "Rebe es una una excelente profesional, durante la clase ella cuida cada movimiento para evitar cualquier lesión; adapta la clase según las necesidades y capacidades de cada persona, desde un nivel básico hasta avanzado. Rebe ha logrado adaptar las clases para ayudarme a ir avanzando sin sentirme frustrada por no poder lograrlo ☺️💪🏻 , esta paciencia y profesionalismo me ha motivado a continuar y seguir avanzando.",
+        image: girl3
     }
 ]
 
